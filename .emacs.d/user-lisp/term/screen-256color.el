87f2d3cc3fc9f2dd1bb4646dae21b3d697486880
Andrew Watts <ahwatts@gmail.com>
2014-02-04 16:41:44 -0500

    Try turning on zenburn again after doing the term colors, as it might have increased the color count.

--- .emacs.d/user-lisp/term/screen-256color.el
+++ .emacs.d/user-lisp/term/screen-256color.el
@@ -1,4 +1,5 @@
 (defun terminal-init-screen ()
   (load "term/xterm")
   (xterm-register-default-colors)
-  (tty-set-up-initial-frame-faces))
+  (tty-set-up-initial-frame-faces)
+  (ahw-turn-on-zenburn))
