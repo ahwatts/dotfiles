2f9b2b47cb41005158fdde931aecb62c41056086
Andrew Watts <ahwatts@gmail.com>
2014-11-23 00:53:17 -0500

    Add slamhound to my Clojure dev dependencies.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,5 @@
 ;; User profiles.
-{:user {:plugins [[lein-ancient "0.5.5"]
+{:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :dependencies [[slamhound "1.5.5"]]
+        :plugins [[lein-ancient "0.5.5"]
                   [cider/cider-nrepl "0.8.0-SNAPSHOT"]]}}
