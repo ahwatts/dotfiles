3ffa97cd700752f2dbd7a2797b393182a60ac288
Andrew Watts <ahwatts@gmail.com>
2014-12-02 11:06:42 -0500

    Use the stable version of cider-nrepl now that it's been released.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,4 +2,4 @@
 {:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :dependencies [[slamhound "1.5.5"]]
         :plugins [[lein-ancient "0.5.5"]
-                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]]}}
+                  [cider/cider-nrepl "0.8.1"]]}}
