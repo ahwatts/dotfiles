ee8e4ce28e1e4a894874fa30dc39d1606d75e6f3
Andrew Watts <ahwatts@gmail.com>
2015-02-13 10:31:40 -0500

    Hide the alt-tab element on OSX.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,6 @@
 ;; User profiles.
 {:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :dependencies [[slamhound "1.5.5"]]
-        :plugins [[lein-ancient "0.5.5"]
-                  [cider/cider-nrepl "0.8.1"]]}}
+        :plugins [[lein-ancient "0.6.2"]
+                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
+        :jvm-opts [(when (= "Mac OS X" (System/getProperty "os.name")) "-Dapple.awt.UIElement=true")]}}
