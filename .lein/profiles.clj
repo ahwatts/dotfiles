aa94a79f531ff3949604f5df7721485dc47bac33
Andrew Watts <ahwatts@gmail.com>
2015-02-22 23:12:22 -0500

    You have to do some fancy stuff to evaluate stuff in your leiningen profiles.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,9 @@
 ;; User profiles.
+;; User profiles.
 {:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :dependencies [[slamhound "1.5.5"]]
         :plugins [[lein-ancient "0.6.2"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
-        :jvm-opts [(when (= "Mac OS X" (System/getProperty "os.name")) "-Dapple.awt.UIElement=true")]}}
+        :jvm-opts #=(eval (vec (filter (complement nil?)
+                                       [(when (= "Mac OS X" (System/getProperty "os.name")) "-Dapple.awt.UIElement=true")
+                                        (when (= "Windows 7" (System/getProperty "os.name")) "-Dline.separator=\"\n\"")])))}}
