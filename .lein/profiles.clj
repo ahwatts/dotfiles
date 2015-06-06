57340be597aa2dd32ab2d33f9d6e9b1958db94b7
Andrew Watts <ahwatts@gmail.com>
2015-06-06 12:15:06 -0400

    Version bumps.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,8 +2,8 @@
 ;; User profiles.
 {:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :dependencies [[slamhound "1.5.5"]
-                       [org.clojure/tools.nrepl "0.2.8"]]
-        :plugins [[lein-ancient "0.6.2"]
+                       [org.clojure/tools.nrepl "0.2.10"]]
+        :plugins [[lein-ancient "0.6.7"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
         :jvm-opts #=(eval (vec (filter (complement nil?)
                                        [(when (= "Mac OS X" (System/getProperty "os.name")) "-Dapple.awt.UIElement=true")
