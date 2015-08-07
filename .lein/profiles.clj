b99f4021789ef6b5c395f1a1948e4a1a995ff818
Andrew Watts <ahwatts@gmail.com>
2015-08-07 01:02:59 -0400

    Updating the Leiningen config.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,14 @@
 ;; User profiles.
-;; User profiles.
-{:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :dependencies [[slamhound "1.5.5"]
-                       [org.clojure/tools.nrepl "0.2.10"]]
-        :plugins [[lein-ancient "0.6.7"]
-                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
-        :jvm-opts #=(eval (vec (filter (complement nil?)
-                                       [(when (= "Mac OS X" (System/getProperty "os.name")) "-Dapple.awt.UIElement=true")
-                                        (when (= "Windows 7" (System/getProperty "os.name")) "-Dline.separator=\"\n\"")])))}}
+{:user
+ {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
+
+  :dependencies [[slamhound "1.5.5"]
+                 [org.clojure/tools.nrepl "0.2.10"]]
+
+  :plugins [[lein-ancient "0.6.7" :exclusions [org.clojure/clojure org.clojure/data.xml]]
+            [cider/cider-nrepl "0.10.0-SNAPSHOT"]]
+
+  :jvm-opts
+  #=(eval (vec (filter (complement nil?)
+                       [(when (= "Mac OS X" (System/getProperty "os.name")) "-Dapple.awt.UIElement=true")
+                        (when (= "Windows 7" (System/getProperty "os.name")) "-Dline.separator=\"\n\"")])))}}
