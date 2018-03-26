d61e77ae0bc4a82268cc4872e5bbb24590d4b341
Andrew Watts <ahwatts@gmail.com>
2018-03-26 14:19:03 -0400

    Bump some deps.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,13 +3,15 @@
  {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
 
   :dependencies [[slamhound "1.5.5"]
-                 [org.clojure/tools.nrepl "0.2.10"]]
+                 [org.clojure/tools.nrepl "0.2.12"]]
 
   :plugins [[lein-ancient "0.6.7" :exclusions [org.clojure/clojure org.clojure/data.xml]]
-            [cider/cider-nrepl "0.14.0"]
+            [cider/cider-nrepl "0.16.0"]
             [lein-pprint "1.1.1"]]
 
   :jvm-opts
   #=(eval (vec (filter (complement nil?)
-                       [(when (= "Mac OS X" (System/getProperty "os.name")) "-Dapple.awt.UIElement=true")
-                        (when (= "Windows 7" (System/getProperty "os.name")) "-Dline.separator=\"\n\"")])))}}
+                       [(when (= "Mac OS X" (System/getProperty "os.name"))
+                          "-Dapple.awt.UIElement=true")
+                        (when (= "Windows 7" (System/getProperty "os.name"))
+                          "-Dline.separator=\"\n\"")])))}}
